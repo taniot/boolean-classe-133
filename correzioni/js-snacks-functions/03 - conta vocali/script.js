99f761cb6a49914599c857d2ff6524bbb6989b39
Gaetano Frascolla <gaetano.frascolla@gmail.com>
2024-10-21 23:55:20 +0200

    ADD folder 133

---
+++ correzioni/js-snacks-functions/03 - conta vocali/script.js
@@ -0,0 +1,9 @@
+/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
+
+const word = 'pizza';
+
+// Dichiara la funzione qui.
+
+// Invoca la funzione qui e stampa il risultato in console
+
+//Risultato atteso se si passa 'javascript': 3 (a, a, i)
